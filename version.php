2e58f600f19a431c4968b8409eb5cb879814c166
Skylar Kelty <S.Kelty@kent.ac.uk>
2013-12-06 15:16:14 +0000

    Define..

--- version.php
+++ version.php
@@ -1,5 +1,8 @@
 <?php
-    $plugin->version = 2013120600;  // YYYYMMDDHH (year, month, day, 24-hr time)
-    $plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
-	$plugin->release = '2.0'; //plugin release
-	$plugin->maturity   = MATURITY_STABLE;	
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2013120600;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
+$plugin->release = '2.0'; //plugin release
+$plugin->maturity   = MATURITY_STABLE;	
